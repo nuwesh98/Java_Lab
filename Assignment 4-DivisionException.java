4d1d5c3cce9860e1cf279f6b8af5e8dc039a676a
nuwesh98 <nuwesh98@gmail.com>
2026-03-13 15:31:14 +0530

    Update Assignment 4-DivisionException.java

--- Assignment 4-DivisionException.java
+++ Assignment 4-DivisionException.java
@@ -1,25 +1,25 @@
-
 import java.util.Scanner;
 
 public class DivisionException {
+
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
 
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+
         try {
-            System.out.print("Enter first integer: ");
-            int a = sc.nextInt();
-
-            System.out.print("Enter second integer: ");
-            int b = sc.nextInt();
-
             int result = a / b;
-            System.out.println("Result of division: " + result);
-        } 
-        catch (ArithmeticException e) {
-            System.out.println("Error: Division by zero is not allowed.");
-        } 
-        finally {
-            sc.close();
+            System.out.println("Result = " + result);
         }
+        catch (ArithmeticException e) {
+            System.out.println("Cannot divide by zero");
+        }
+
+        sc.close();
     }
 }
