637442a071f45cbe734af5b8a9a8c4067377c076
nuwesh98 <nuwesh98@gmail.com>
2026-03-13 15:25:40 +0530

    Update Assignment 4-DivisionException.java

--- Assignment 4-DivisionException.java
+++ Assignment 4-DivisionException.java
@@ -1,7 +1,7 @@
 
 import java.util.Scanner;
 
-public class Assignment4_DivisionException {
+public class DivisionException {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
