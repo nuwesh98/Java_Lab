0cd70dc2be629c18cfb9bbed529d1692a43d7268
nuwesh98 <nuwesh98@gmail.com>
2026-03-13 15:31:54 +0530

    Update Assignment 4-AWTImageText.java

--- Assignment 4-AWTImageText.java
+++ Assignment 4-AWTImageText.java
@@ -1,35 +1,27 @@
-
 import java.awt.*;
-import java.awt.event.*;
 
 public class AWTImageText extends Frame {
 
     Image img;
 
-    public Assignment4_AWTImageText() {
-        setTitle("AWT Image and Text Display");
-        setSize(500, 400);
+    Assignment4_AWTImageText() {
+
+        setTitle("AWT Example");
+        setSize(400,300);
         setVisible(true);
 
         img = Toolkit.getDefaultToolkit().getImage("image.jpg");
-
-        addWindowListener(new WindowAdapter() {
-            public void windowClosing(WindowEvent e) {
-                dispose();
-            }
-        });
     }
 
     public void paint(Graphics g) {
-        g.setFont(new Font("Arial", Font.BOLD, 20));
-        g.drawString("Welcome to AWT Application", 120, 80);
 
-        if (img != null) {
-            g.drawImage(img, 180, 120, 150, 150, this);
-        }
+        g.drawString("Hello AWT Application",150,80);
+
+        g.drawImage(img,120,120,100,100,this);
     }
 
-    public static void main(String[] args) {
-        new Assignment4_AWTImageText();
+    public static void main(String args[]) {
+
+        new AWTImageText();
     }
 }
